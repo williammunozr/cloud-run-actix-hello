ffb509e8a4883dcb4dd516d6e09198b13fbe97d7
william <william.munozr@gmail.com>
2023-03-28 09:09:47 -0500

    Add env variable to get the port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port= match std::env::var("PORT") {
+        Ok(port) => port,
+        _ => String::from("8080")
+    };
+
+    let address = format!("0.0.0.0:{}", port);
+
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(address.clone())?
+        .run()
+        .await
 }
